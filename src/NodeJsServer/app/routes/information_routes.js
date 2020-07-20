67f5145adc5a3f818b846839efff3bd64346ff95
Sabina Alikilichova <sab.vkolokol@gmail.com>
2020-07-20 11:48:48 +0300

    post

--- src/NodeJsServer/app/routes/information_routes.js
+++ src/NodeJsServer/app/routes/information_routes.js
@@ -1,16 +1,28 @@
+bodyParser = require('body-parser').json();
 module.exports = function (app) {
     app.get('/information', (request, response) => {
-        const result = [{
+        const result = {
             "city": "Казань",
             "age": 19,
-            "citizenship" : "РФ",
-            "educ" : "КФУ ВШ ИТИС"
-        },
-            {
-                "computer_skills": "Java, python",
-                "bad_habits": "Курение",
-                "vk" : "https://vk.com/posh.ebosh"
-            }];
+            "citizenship": "РФ",
+            "educ": "КФУ ВШ ИТИС",
+            "computer_skills": "Java, python",
+            "bad_habits": "Курение",
+            "vk": "https://vk.com/posh.ebosh"
+        };
         response.send(JSON.stringify(result));
+    });
+    const  fs = require("fs");
+    app.post('/information', bodyParser, (request, response)=>{
+        let body = response.body;
+        let responseBody = {
+            "email": body["email"]
+        }
+        fs.appendFile("email.txt",JSON.stringify(responseBody), function (error) {
+            if(error) throw error;
+        })
+        response.setHeader("Content-Type", "application/json");
+        response.send(JSON.stringify(responseBody));
+
     });
 };
